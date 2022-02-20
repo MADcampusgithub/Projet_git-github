2a9dcfb370f3af5b658ec6870822abe94f840024
MADcampusgithub <maxime.adler@campus-la-chataigneraie.org>
2022-02-20 15:07:31 +0100

    Création de la méthode post

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	m "Projet_git-github/models"
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -17,4 +20,46 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func PostContact(c *gin.Context) {
+	content, err := ioutil.ReadFile("contacts.json")
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not read contacts file")
+		log.Fatal(err)
+	}
+
+	var contacts []m.Contact
+	err = json.Unmarshal(content, &contacts)
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not parse contacts file")
+		log.Fatal(err)
+	}
+
+	var message m.Contact
+	err = c.ShouldBindJSON(&message)
+
+	if err != nil {
+		c.String(http.StatusBadRequest, "Data could not be parsed ")
+		return
+	}
+
+	contacts = append(contacts, message)
+	content, err = json.Marshal(contacts)
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not parse contacts")
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("contacts.json", content, 0644)
+
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Could not write contacts in file")
+		log.Fatal(err)
+	}
+
+	c.String(http.StatusOK, "Ok")
 }
